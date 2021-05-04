262a965453c9e60b1b4c12c98bc06f7819cca968
Dustin Tran <dustin.andy.tran@gmail.com>
2021-05-04 16:20:55 +0900

    feat: implemented OIDC get tokens
    Co-authored-by: Tsubasa Kondo <TsubasaK111@users.noreply.github.com>

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,50 +1,27 @@
 <template>
   <v-container>
-      <v-btn v-on:click="oauthLogin">
-          Login!
-      </v-btn>
+      <v-btn :href=twitchOAuthRegisterURL>Login A Href</v-btn>
   </v-container>
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 
 export default {
   name: "Login",
 
   mounted: async function () {
+    this.twitchOAuthRegisterURL = `https://id.twitch.tv/oauth2/authorize?client_id=${process.env.VUE_APP_CLIENTID}&redirect_uri=http://localhost:8080&response_type=id_token&scope=openid`
 
-    console.log("mounted!");
-    const result = await axios.get(`/api`);
-
-    console.log(result);
+    // console.log("mounted!");
+    // const result = await axios.get(`/api`);
+    // console.log(result);
   },
 
   data: () => ({
-    
+    twitchOAuthRegisterURL: ""
   }),
   methods:{
-      oauthLogin: function(event){
-          const options = {
-              method: 'GET',
-              url: 'https://id.twitch.tv/oauth2/authorize',
-              headers: {
-                  "Access-Control-Allow-Origin": "http://localhost:8080"
-              },
-              params: {
-                  client_id: process.env.VUE_APP_CLIENTID || "",
-                  redirect_uri: "http://localhost:8080",
-                  response_type: "id_token",
-                  scope: "open_id",
-                  claims: ""
-              }
-          }
-          
-          axios.request(options)
-
-          //start with authorization URL
-          //add client id
-      }
   }
 };
 </script>
