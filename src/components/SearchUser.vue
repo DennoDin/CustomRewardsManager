a285e89bd6bfe6e78abb13ce33a7363f4e4558b0
Dustin Tran <dustin.andy.tran@gmail.com>
2021-05-06 20:22:55 +0900

    WIP: created SearchUser component base

---
+++ src/components/SearchUser.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <v-text-field v-bind=username input label="Check Status of a User"/>
+        <v-btn>Search</v-btn>
+    </div>
+</template>
+<script>
+export default {
+    name: "SearchUser",
+    data: () => ({
+        username: ""
+    }),
+}
+</script>
