d16454a81a4af0a536e4ed6dce318a47dee4be42
Dustin Tran <dustin.andy.tran@gmail.com>
2021-05-13 12:50:53 +0900

    feat: Captured URL Fragment including Access Token
    Co-authored-by: Tsubasa Kondo <TsubasaK111@users.noreply.github.com>

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,18 +11,21 @@ export default {
   name: "Login",
 
   mounted: async function () {
-    this.twitchOAuthRegisterURL = `https://id.twitch.tv/oauth2/authorize?client_id=${process.env.VUE_APP_CLIENTID}&redirect_uri=http://localhost:8080&response_type=id_token&scope=openid`
+    this.twitchOAuthRegisterURL = `https://id.twitch.tv/oauth2/authorize?client_id=${process.env.VUE_APP_CLIENTID}&redirect_uri=http://localhost:8080&response_type=token+id_token&scope=openid`
 
     // console.log("mounted!");
     // const result = await axios.get(`/api`);
     // console.log(result);
 
-    this.token = window.location.hash.substring(10);
+    const urlFragment = new URLSearchParams(window.location.hash.substring(1));
+    for(let pair of urlFragment.entries()) {
+      this.fragmentResults[pair[0]] =  pair[1];
+    }
   },
 
   data: () => ({
     twitchOAuthRegisterURL: "",
-    token: "",
+    fragmentResults: {},
   }),
   methods:{
   }
