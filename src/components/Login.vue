2dd4c58697787e6f31f8f1c5b86566b1cf0899cd
Dustin Tran <dustin.andy.tran@gmail.com>
2021-05-31 20:19:31 +0900

    clean: remove redirect code from login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -27,27 +27,12 @@ export default {
     // console.log("mounted!");
     // const result = await axios.get(`/api`);
     // console.log(result);
-
-    const urlFragment = new URLSearchParams(window.location.hash.substring(1));
-    for(let pair of urlFragment.entries()) {
-      this.fragmentResults[pair[0]] =  pair[1];
-    }
-    if(this.fragmentResults.token_type && this.fragmentResults.token_type === 'bearer'){
-      this.fragmentResults.token_type = "Bearer"
-    }
-    if(Object.keys(this.fragmentResults).length){
-      this.updateCredentials(this.fragmentResults)
-    }
   },
   data: () => ({
     twitchOAuthRegisterURL: "",
     fragmentResults: {},
   }),
   methods:{
-    updateCredentials(returnedCreds){
-      this.$store.commit('saveCredentials', returnedCreds)
-      this.$store.commit('toggleLogin')
-    }
   }
 };
 </script>
