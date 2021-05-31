4867abe6a851fb80ba1e4b9842ab4b6e82539886
Dustin Tran <dustin.andy.tran@gmail.com>
2021-05-31 18:25:13 +0900

    added Vue Router

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <login/>
+    <search-channel/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '../components/Login.vue'
+import SearchChannel from '../components/SearchChannel.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Login,
+    SearchChannel
+  }
+}
+</script>
