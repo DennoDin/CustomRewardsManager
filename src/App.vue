03ebcd954a54c686c194be6850cd0b280df13ba8
Dustin Tran <dustin.andy.tran@gmail.com>
2021-05-31 13:34:46 +0900

    clean: removed unused data and methods

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,38 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <span>TSTKT</span>
     </v-app-bar>
 
     <v-main>
-      <div tag="Login" v-if=!isLoggedIn>
-        <Login/>
+      <div tag="Login" v-if="!isLoggedIn">
+        <Login />
       </div>
-      <div v-if=isLoggedIn>
-        <Search-Channel v-bind:creds="credentials"/>
+      <div v-if="isLoggedIn">
+        <Search-Channel />
       </div>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Login from './components/Login';
-import SearchChannel from './components/SearchChannel'
+import Login from "./components/Login";
+import SearchChannel from "./components/SearchChannel";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     Login,
     SearchChannel,
   },
 
-  data: () => ({
-    isAuthenticated: false,
-    credentials: {},  //store credentials in cookies at a later point
-  }),
-  methods:{
-    updateCreds(returnedCredentials){
-      if(Object.keys(returnedCredentials).length > 0){
-        Object.assign(this.credentials, returnedCredentials)
-        this.isAuthenticated = true;
-      }
-      if(this.credentials.token_type === "bearer"){
-        this.credentials.token_type = "Bearer"
-      }
-    }
+  data: () => ({}),
+  methods: {},
+  computed: {
+    isLoggedIn() {
+      return this.$store.state.isLoggedIn;
+    },
   },
-  computed:{
-    isLoggedIn(){
-      return this.$store.state.isLoggedIn
-    }
-  }
 };
 </script>
